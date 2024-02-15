8448bed43be5d09ee710221cf33feb11c09aab19
yongjunlee <93720125+2hapu@users.noreply.github.com>
2024-02-15 23:58:23 +0900

    test: EventControllerTest 추가

---
+++ src/test/java/com/happiday/Happi_Day/controller/EventControllerTest.java
@@ -0,0 +1,32 @@
+package com.happiday.Happi_Day.controller;
+
+import com.happiday.Happi_Day.domain.controller.EventController;
+import com.happiday.Happi_Day.domain.service.UserService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+@ExtendWith(MockitoExtension.class)
+public class EventControllerTest {
+
+    @InjectMocks
+    private EventController eventController;
+
+    @Mock
+    private UserService userService;
+
+    private MockMvc mockMvc;
+
+    @BeforeEach
+    public void init() {
+        mockMvc = MockMvcBuilders.standaloneSetup(eventController).build();
+    }
+
+
+}
