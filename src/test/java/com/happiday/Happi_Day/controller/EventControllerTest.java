79283e79273d559cb423588dfa7873e53d4e2e22
yongjunlee <93720125+2hapu@users.noreply.github.com>
2024-02-23 23:59:18 +0900

    remove: import 삭제

--- src/test/java/com/happiday/Happi_Day/controller/EventControllerTest.java
+++ src/test/java/com/happiday/Happi_Day/controller/EventControllerTest.java
@@ -4,7 +4,6 @@ import com.happiday.Happi_Day.domain.controller.EventController;
 import com.happiday.Happi_Day.domain.service.UserService;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
