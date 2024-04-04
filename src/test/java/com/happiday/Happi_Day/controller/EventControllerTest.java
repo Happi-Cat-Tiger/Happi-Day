fda9b7223039bd22fd7ce2cd7b756769734e77ed
yongjunlee <93720125+2hapu@users.noreply.github.com>
2024-04-04 21:37:29 +0900

    test: EventControllerTest 추가

--- src/test/java/com/happiday/Happi_Day/controller/EventControllerTest.java
+++ src/test/java/com/happiday/Happi_Day/controller/EventControllerTest.java
@@ -1,12 +1,16 @@
 package com.happiday.Happi_Day.controller;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import com.happiday.Happi_Day.domain.controller.EventController;
 import com.happiday.Happi_Day.domain.controller.UserController;
 import com.happiday.Happi_Day.domain.entity.article.Hashtag;
 import com.happiday.Happi_Day.domain.entity.artist.Artist;
 import com.happiday.Happi_Day.domain.entity.event.Event;
 import com.happiday.Happi_Day.domain.entity.event.dto.EventCreateDto;
+import com.happiday.Happi_Day.domain.entity.event.dto.EventListResponseDto;
 import com.happiday.Happi_Day.domain.entity.event.dto.EventResponseDto;
+import com.happiday.Happi_Day.domain.entity.event.dto.EventUpdateDto;
 import com.happiday.Happi_Day.domain.entity.team.Team;
 import com.happiday.Happi_Day.domain.entity.user.RoleType;
 import com.happiday.Happi_Day.domain.entity.user.User;
@@ -23,8 +27,8 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.*;
+import org.springframework.data.web.PageableHandlerMethodArgumentResolver;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.MediaType;
 import org.springframework.mock.web.MockMultipartFile;
@@ -44,10 +48,12 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
+import static org.hamcrest.Matchers.containsString;
 import static org.mockito.ArgumentMatchers.*;
 import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 
@@ -63,7 +69,8 @@ public class EventControllerTest {
     @Mock
     private EventService eventService;
 
-
+    private ObjectMapper objectMapper = new ObjectMapper()
+            .registerModule(new JavaTimeModule());
     private User user1;
 
     private Artist artist1, artist2;
@@ -72,9 +79,8 @@ public class EventControllerTest {
 
     private Hashtag hashtag1, hashtag2;
 
-    private MockMultipartFile thumbnailFile, imageFile;
+    private MockMultipartFile thumbnailFile, imageFile, updateThumbnailFile, updateImageFile;
 
-    private Event event1;
 
     private EventResponseDto mockEventResponseDto;
 
@@ -82,7 +88,9 @@ public class EventControllerTest {
 
     @BeforeEach
     public void init() {
-        mockMvc = MockMvcBuilders.standaloneSetup(eventController).build();
+        mockMvc = MockMvcBuilders.standaloneSetup(eventController)
+                .setCustomArgumentResolvers(new PageableHandlerMethodArgumentResolver())
+                .build();
     }
 
     @BeforeAll
@@ -142,18 +150,10 @@ public class EventControllerTest {
         imageFile = new MockMultipartFile(
                 "imageFile", "eventImage2.jpg", MediaType.IMAGE_JPEG_VALUE, "ImageData".getBytes());
 
-//        event1 = Event.builder()
-//                .title("제목")
-//                .user(user1)
-//                .startTime(LocalDateTime.now().minusMonths(1))
-//                .endTime(LocalDateTime.now().plusMonths(3))
-//                .description("내용")
-//                .address("서울특별시 서초구 반포대로30길 32")
-//                .location("1층 카페 이로")
-//                .imageUrl(String.valueOf(eventImage1))
-//                .eventHashtags(new ArrayList<>())
-//                .build();
-
+        updateThumbnailFile = new MockMultipartFile(
+                "thumbnailFile", "newEventImage1.jpg", MediaType.IMAGE_JPEG_VALUE, "ImageData".getBytes());
+        updateImageFile = new MockMultipartFile(
+                "imageFile", "newEventImage2.jpg", MediaType.IMAGE_JPEG_VALUE, "ImageData".getBytes());
 
         LocalDateTime startTime = LocalDateTime.parse("2023-08-31T10:00:00", DateTimeFormatter.ISO_LOCAL_DATE_TIME);
         LocalDateTime endTime = LocalDateTime.parse("2023-09-23T18:00:00", DateTimeFormatter.ISO_LOCAL_DATE_TIME);
@@ -161,7 +161,7 @@ public class EventControllerTest {
         mockEventResponseDto = EventResponseDto.builder()
                 .id(1L)
                 .username(user1.getUsername())
-                .title("좋은 이벤트")
+                .title("good event")
                 .startTime(startTime)
                 .endTime(endTime)
                 .description("좋은 이벤트 설명")
@@ -177,7 +177,7 @@ public class EventControllerTest {
 
     private String eventJsonRequest() {
         return "{"
-                + "\"title\":\"좋은 이벤트\","
+                + "\"title\":\"good event\","
                 + "\"startTime\":\"2023-08-31T10:00:00\","
                 + "\"endTime\":\"2023-09-23T18:00:00\","
                 + "\"description\":\"좋은 이벤트 설명\","
@@ -211,7 +211,7 @@ public class EventControllerTest {
         // given
         Long testEventId = 1L;
         when(eventService.readEvent(anyString(), eq(testEventId)))
-                .thenReturn(mockEventResponseDto); // 서비스 메소드 호출 결과 모킹
+                .thenReturn(mockEventResponseDto);
 
         // when, then
         mockMvc.perform(get("/api/v1/events/" + testEventId))
@@ -231,137 +231,191 @@ public class EventControllerTest {
         mockMvc.perform(delete("/api/v1/events/" + testEventId))
                 .andExpect(status().isOk());
     }
-//
-//    @Test
-//    @DisplayName("이벤트 목록 조회 API 테스트")
-//    void readEventsTest() throws Exception {
-//        // given
-//        Pageable pageable = PageRequest.of(0, 10); // 페이지 정보 설정
-//        String filter = "someFilter"; // 필터 설정
-//        String keyword = "someKeyword"; // 키워드 설정
-//
-//        Page<EventListResponseDto> mockResponseDtoPage = ...; // Mock 데이터 생성
-//
-//        // 이벤트 서비스의 readEvents 메서드가 호출될 때 모의 객체가 반환할 데이터 설정
-//        when(eventService.readEvents(pageable, filter, keyword)).thenReturn(mockResponseDtoPage);
-//
-//        // when, then
-//        mockMvc.perform(get("/api/v1/events")
-//                        .param("filter", filter)
-//                        .param("keyword", keyword))
-//                .andExpect(status().isOk());
-//    }
-//
-//    @Test
-//    @DisplayName("진행 중인 이벤트 목록 조회 API 테스트")
-//    void readOngoingEventsTest() throws Exception {
-//        // given
-//        Pageable pageable = PageRequest.of(0, 10); // 페이지 정보 설정
-//        String filter = "someFilter"; // 필터 설정
-//        String keyword = "someKeyword"; // 키워드 설정
-//
-//        Page<EventListResponseDto> mockResponseDtoPage = ...; // Mock 데이터 생성
-//
-//        // 이벤트 서비스의 readOngoingEvents 메서드가 호출될 때 모의 객체가 반환할 데이터 설정
-//        when(eventService.readOngoingEvents(pageable, filter, keyword)).thenReturn(mockResponseDtoPage);
-//
-//        // when, then
-//        mockMvc.perform(get("/api/v1/events/ongoing")
-//                        .param("filter", filter)
-//                        .param("keyword", keyword))
-//                .andExpect(status().isOk());
-//    }
-//
-//    @Test
-//    @DisplayName("구독한 아티스트/팀의 이벤트 목록 조회 API 테스트")
-//    void readEventsBySubscribedArtistsTest() throws Exception {
-//        // given
-//        Pageable pageable = PageRequest.of(0, 10); // 페이지 정보 설정
-//        String filter = "someFilter"; // 필터 설정
-//        String keyword = "someKeyword"; // 키워드 설정
-//
-//        Page<EventListResponseDto> mockResponseDtoPage = ...; // Mock 데이터 생성
-//
-//        // 이벤트 서비스의 readEventsBySubscribedArtists 메서드가 호출될 때 모의 객체가 반환할 데이터 설정
-//        when(eventService.readEventsBySubscribedArtists(pageable, filter, keyword, anyString())).thenReturn(mockResponseDtoPage);
-//
-//        // when, then
-//        mockMvc.perform(get("/api/v1/events/subscribedArtists")
-//                        .param("filter", filter)
-//                        .param("keyword", keyword))
-//                .andExpect(status().isOk());
-//    }
-//
-//    @Test
-//    @DisplayName("구독한 아티스트/팀의 진행 중인 이벤트 목록 조회 API 테스트")
-//    void readOngoingEventsBySubscribedArtistsTest() throws Exception {
-//        // given
-//        Pageable pageable = PageRequest.of(0, 10); // 페이지 정보 설정
-//        String filter = "someFilter"; // 필터 설정
-//        String keyword = "someKeyword"; // 키워드 설정
-//
-//        Page<EventListResponseDto> mockResponseDtoPage = ...; // Mock 데이터 생성
-//
-//        // 이벤트 서비스의 readOngoingEventsBySubscribedArtists 메서드가 호출될 때 모의 객체가 반환할 데이터 설정
-//        when(eventService.readOngoingEventsBySubscribedArtists(pageable, filter, keyword, anyString())).thenReturn(mockResponseDtoPage);
-//
-//        // when, then
-//        mockMvc.perform(get("/api/v1/events/subscribedArtists/ongoing")
-//                        .param("filter", filter)
-//                        .param("keyword", keyword))
-//                .andExpect(status().isOk());
-//    }
-//
-//    @Test
-//    @DisplayName("이벤트 수정 API 테스트")
-//    void updateEventTest() throws Exception {
-//        // given
-//        Long testEventId = 1L; // 테스트할 이벤트 ID
-//        EventUpdateDto eventUpdateDto = ...; // 이벤트 업데이트 DTO 생성
-//        MockMultipartFile thumbnailFile = ...; // 썸네일 파일 생성
-//        MockMultipartFile imageFile = ...; // 이미지 파일 생성
-//
-//        EventResponseDto mockResponseDto = ...; // Mock 응답 DTO 생성
-//
-//        // 이벤트 서비스의 updateEvent 메서드가 호출될 때 모의 객체가 반환할 데이터 설정
-//        when(eventService.updateEvent(eq(testEventId), eq(eventUpdateDto), eq(thumbnailFile), eq(imageFile), anyString())).thenReturn(mockResponseDto);
-//
-//        // when, then
-//        mockMvc.perform(put("/api/v1/events/" + testEventId)
-//                        .param("thumbnailFile", "thumbnail.jpg")
-//                        .param("imageFile", "image.jpg")
-//                        .contentType(MediaType.MULTIPART_FORM_DATA)
-//                        .content(objectMapper.writeValueAsString(eventUpdateDto)))
-//                .andExpect(status().isOk());
-//    }
-//
-//    @Test
-//    @DisplayName("이벤트 좋아요 API 테스트")
-//    void likeEventTest() throws Exception {
-//        // given
-//        Long testEventId = 1L; // 테스트할 이벤트 ID
-//
-//        // 이벤트 서비스의 likeEvent 메서드가 호출될 때 모의 객체가 반환할 데이터 설정
-//        when(eventService.likeEvent(eq(testEventId), anyString())).thenReturn("좋아요 성공 메시지");
-//
-//        // when, then
-//        mockMvc.perform(post("/api/v1/events/" + testEventId + "/like"))
-//                .andExpect(status().isOk());
-//    }
-//
-//    @Test
-//    @DisplayName("이벤트 참가하기 API 테스트")
-//    void joinEventTest() throws Exception {
-//        // given
-//        Long testEventId = 1L; // 테스트할 이벤트 ID
-//
-//        // 이벤트 서비스의 joinEvent 메서드가 호출될 때 모의 객체가 반환할 데이터 설정
-//        when(eventService.joinEvent(eq(testEventId), anyString())).thenReturn("참가 성공 메시지");
-//
-//        // when, then
-//        mockMvc.perform(post("/api/v1/events/" + testEventId + "/join"))
-//                .andExpect(status().isOk());
-//    }
+
+    private Page<EventListResponseDto> eventMockListResponseDtoPages() {
+
+        List<EventListResponseDto> mockResponseDtoList = new ArrayList<>();
+
+        EventListResponseDto eventListDto1 = EventListResponseDto.builder()
+                .id(1L)
+                .nickname(user1.getUsername())
+                .title("이벤트 제목 1")
+                .updatedAt(LocalDateTime.now())
+                .startTime(LocalDateTime.now())
+                .endTime(LocalDateTime.now().plusHours(2))
+                .location("서울")
+                .thumbnailUrl(thumbnailFile.getName())
+                .artists(List.of(artist1.getName(), artist2.getName()))
+                .teams(List.of(team1.getName()))
+                .hashtags(List.of(hashtag1.getTag(), hashtag2.getTag()))
+                .build();
+
+        EventListResponseDto eventListDto2 = EventListResponseDto.builder()
+                .id(2L)
+                .nickname(user1.getUsername())
+                .title("이벤트 제목 2")
+                .updatedAt(LocalDateTime.now())
+                .startTime(LocalDateTime.now())
+                .endTime(LocalDateTime.now().plusHours(3))
+                .location("경기")
+                .thumbnailUrl(thumbnailFile.getName())
+                .artists(List.of(artist2.getName()))
+                .teams(List.of(team2.getName()))
+                .hashtags(List.of(hashtag1.getTag(), hashtag2.getTag()))
+                .build();
+
+        mockResponseDtoList.add(eventListDto1);
+        mockResponseDtoList.add(eventListDto2);
+
+        Page<EventListResponseDto> mockResponseDtoPage = new PageImpl<>(mockResponseDtoList);
+
+        return mockResponseDtoPage;
+    }
+    @Test
+    @DisplayName("이벤트 목록 조회 API 테스트")
+    void readEventsTest() throws Exception {
+        // given
+        Pageable pageable = PageRequest.of(0, 10, Sort.by(Sort.Direction.DESC, "id"));
+        String filter = "testFilter";
+        String keyword = "testKeyword";
+
+        Page<EventListResponseDto> mockResponseDtoPage = eventMockListResponseDtoPages();
+
+        when(eventService.readEvents(pageable, filter, keyword)).thenReturn(mockResponseDtoPage);
+
+        // when, then
+        mockMvc.perform(get("/api/v1/events")
+                        .param("filter", filter)
+                        .param("keyword", keyword))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    @DisplayName("진행 중인 이벤트 목록 조회 API 테스트")
+    void readOngoingEventsTest() throws Exception {
+        // given
+        Pageable pageable = PageRequest.of(0, 10, Sort.by(Sort.Direction.DESC, "id"));
+        String filter = "testFilter";
+        String keyword = "testKeyword";
+
+        Page<EventListResponseDto> mockResponseDtoPage = eventMockListResponseDtoPages();
+
+        when(eventService.readOngoingEvents(pageable, filter, keyword)).thenReturn(mockResponseDtoPage);
+
+        // when, then
+        mockMvc.perform(get("/api/v1/events/ongoing")
+                        .param("filter", filter)
+                        .param("keyword", keyword))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    @DisplayName("구독한 아티스트/팀의 이벤트 목록 조회 API 테스트")
+    void readEventsBySubscribedArtistsTest() throws Exception {
+        // given
+        Pageable pageable = PageRequest.of(0, 10, Sort.by(Sort.Direction.DESC, "id"));
+        String filter = "testFilter";
+        String keyword = "testKeyword";
+
+        Page<EventListResponseDto> mockResponseDtoPage = eventMockListResponseDtoPages();
+
+        when(eventService.readEventsBySubscribedArtists(pageable, filter, keyword, user1.getUsername())).thenReturn(mockResponseDtoPage);
+
+        // when, then
+        mockMvc.perform(get("/api/v1/events/subscribedArtists")
+                        .param("filter", filter)
+                        .param("keyword", keyword))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    @DisplayName("구독한 아티스트/팀의 진행 중인 이벤트 목록 조회 API 테스트")
+    void readOngoingEventsBySubscribedArtistsTest() throws Exception {
+        // given
+        Pageable pageable = PageRequest.of(0, 10, Sort.by(Sort.Direction.DESC, "id"));
+        String filter = "testFilter";
+        String keyword = "testKeyword";
+
+        Page<EventListResponseDto> mockResponseDtoPage = eventMockListResponseDtoPages();
+
+        when(eventService.readOngoingEventsBySubscribedArtists(pageable, filter, keyword, user1.getUsername())).thenReturn(mockResponseDtoPage);
+
+        // when, then
+        mockMvc.perform(get("/api/v1/events/subscribedArtists/ongoing")
+                        .param("filter", filter)
+                        .param("keyword", keyword))
+                .andExpect(status().isOk());
+    }
+
+    private EventUpdateDto eventMockUpdateDto() {
+        return EventUpdateDto.builder()
+                .title("이벤트 바뀐 제목")
+                .startTime(LocalDateTime.now())
+                .endTime(LocalDateTime.now().plusHours(2))
+                .description("수정된 내용")
+                .address("수정된 주소")
+                .location("서울")
+                .thumbnailUrl(thumbnailFile.getName())
+                .hashtags(List.of(hashtag1.getTag(), hashtag2.getTag(), "테스트해시태그", "아이유"))
+                .build();
+
+    }
+
+    @Test
+    @DisplayName("이벤트 수정 API 테스트")
+    void updateEventTest() throws Exception {
+        // given
+        Long testEventId = 1L;
+        EventUpdateDto eventUpdateDto = eventMockUpdateDto();
+
+        MockMultipartFile eventDtoAsJson = new MockMultipartFile("event", "", "application/json", objectMapper.writeValueAsBytes(eventUpdateDto));
+
+        when(eventService.updateEvent(eq(testEventId), any(EventUpdateDto.class), any(), any(), eq(user1.getUsername())))
+                .thenReturn(mockEventResponseDto);
+
+        // when, then
+        mockMvc.perform(multipart(HttpMethod.PUT, "/api/v1/events/{eventId}", testEventId)
+                        .file(eventDtoAsJson)
+                        .file(updateThumbnailFile)
+                        .file(updateThumbnailFile)
+                        .contentType(MediaType.MULTIPART_FORM_DATA_VALUE))
+                .andExpect(status().isOk())
+                .andExpect(content().json(objectMapper.writeValueAsString(mockEventResponseDto)));
+    }
+
+    @Test
+    @DisplayName("이벤트 좋아요 API 테스트")
+    void likeEventTest() throws Exception {
+        // given
+        Long testEventId = 1L;
+        String expectedResponse = "like / likecount : " + 1;
+
+        when(eventService.likeEvent(testEventId, user1.getUsername())).thenReturn(expectedResponse);
+
+        // when, then
+        mockMvc.perform(post("/api/v1/events/{eventId}/like", testEventId))
+                .andExpect(status().isOk())
+                .andDo(print())
+                .andExpect(content().string(containsString(expectedResponse)));
+    }
+
+    @Test
+    @DisplayName("이벤트 참가하기 API 테스트")
+    void joinEventTest() throws Exception {
+        // given
+        Long testEventId = 1L;
+        String expectedResponse = mockEventResponseDto.getTitle() + "participation";
+
+        when(SecurityUtils.getCurrentUsername()).thenReturn(user1.getUsername());
+
+        when(eventService.joinEvent(testEventId, user1.getUsername())).thenReturn(expectedResponse);
+
+        // when, then
+        mockMvc.perform(post("/api/v1/events/" + testEventId + "/join"))
+                .andExpect(status().isOk())
+                .andDo(print())
+                .andExpect(content().string(containsString(expectedResponse)));
+    }
 
 
 
